e167c70215745cda6394bbc36702be3224512aaa
Thomas Allen <tomtinderbundle@gmail.com>
2020-12-10 22:42:18 -0800

    quiz 7

--- Assets/scripts/MusicPlayer.cs
+++ Assets/scripts/MusicPlayer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour
+public class musicPlayer : MonoBehaviour
 {
     private void Awake()
     {
